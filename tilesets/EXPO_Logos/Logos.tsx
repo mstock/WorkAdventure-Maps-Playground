a1d2f1f1a917126f0fa5184f889131e30a4f35d3
Momen-Mawad <Momen-Mawad@users.noreply.github.com>
2021-05-17 21:08:27 +0200

    Erster Entwurf von Expo-Raum

---
+++ tilesets/EXPO_Logos/Logos.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Logos" tilewidth="302" tileheight="151" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="302" height="151" source="01_C2C.png"/>
+ </tile>
+ <tile id="1">
+  <image width="302" height="151" source="02_WG.png"/>
+ </tile>
+ <tile id="2">
+  <image width="302" height="151" source="03_terrestris.png"/>
+ </tile>
+ <tile id="3">
+  <image width="302" height="151" source="04_mundialis.png"/>
+ </tile>
+ <tile id="4">
+  <image width="302" height="151" source="05_geOps.png"/>
+ </tile>
+ <tile id="5">
+  <image width="302" height="151" source="06_GBD.png"/>
+ </tile>
+ <tile id="6">
+  <image width="302" height="151" source="07_mapTiler.png"/>
+ </tile>
+ <tile id="7">
+  <image width="302" height="151" source="08_ESRI.png"/>
+ </tile>
+ <tile id="8">
+  <image width="302" height="151" source="09_GEOINFO.png"/>
+ </tile>
+ <tile id="9">
+  <image width="302" height="151" source="10_OpenCage.png"/>
+ </tile>
+ <tile id="10">
+  <image width="302" height="151" source="11_SP.png"/>
+ </tile>
+ <tile id="11">
+  <image width="302" height="151" source="12_GeoCockpit.png"/>
+ </tile>
+ <tile id="12">
+  <image width="302" height="151" source="13_LATLON.png"/>
+ </tile>
+ <tile id="13">
+  <image width="302" height="151" source="14_FOSSGISeV.png"/>
+ </tile>
+ <tile id="15">
+  <image width="302" height="151" source="16_OSGeo.png"/>
+ </tile>
+ <tile id="16">
+  <image width="302" height="151" source="17_OSGeoLive.png"/>
+ </tile>
+ <tile id="17">
+  <image width="154" height="149" source="15_OSM.png"/>
+ </tile>
+</tileset>
---
+++ tilesets/Tiles_fuer_Expo/Floor - Collision.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Floor - Collision" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="floortileset_fliese - Collision up.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0.125" width="31.875" height="3.625"/>
+  </objectgroup>
+ </tile>
+</tileset>
